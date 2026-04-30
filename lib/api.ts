e2bbeb7fb44823370112a4fba32c354b92582519
Joseph Joshua <calebayuba222@gmail.com>
2026-04-30 15:48:38 +0100

    fix: user route

--- lib/api.ts
+++ lib/api.ts
@@ -34,7 +34,7 @@ export async function githubLogin() {
 }
 
 export async function getMe() {
-	const res = await apiFetch('/users/me');
+	const res = await apiFetch('/auth/me');
 	const data = await res.json();
 	return data.data;
 }
